422973dce636510363e1a70628e5db03a8d61414
srini <srini@LAPTOP-UMBR0RMU>
2022-01-05 19:11:10 +0530

    Committed bro

---
+++ SpringBootRest-3/src/main/java/com/th/constants/PropertyConstants.java
@@ -0,0 +1,16 @@
+package com.th.constants;
+
+/**
+ * 
+ * @author venkatesh_devale
+ *
+ */
+
+public class PropertyConstants {
+
+	public static final String GET_ALL_GROCERY = "/getallgrocery";
+	public static final String SIGN_UP = "/signup";
+	
+	
+
+}
